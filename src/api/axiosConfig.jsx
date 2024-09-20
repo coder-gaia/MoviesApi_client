233407cb56871f7661fed6e71398d10904f03bcd
coder-gaia <alexandregaia911@gmail.com>
2024-09-20 20:47:20 -0300

    test

--- src/api/axiosConfig.jsx
+++ src/api/axiosConfig.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://movies-app-client-side.onrender.com",
+  baseURL: "https://movies-app-client-side.onrender.com/api/v1",
   headers: { "ngrok-skip-browser-warning": "true" },
 });
